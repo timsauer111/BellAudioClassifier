25c2064fd88e87f10571852e810f1ffa5e35d8a9
Tim Sauer <Mail@tim-Sauer.com>
2025-01-29 15:42:04 +0100

    Refactor tkinter UI integration, update classifier initialization, and enhance audio classification logic; Classifikator is now linked to UI start button

--- classifier.py
+++ classifier.py
@@ -14,75 +14,75 @@ from scipy.io import wavfile
 
 from soundReader import Recorder
 
-AudioClassifier = mp.tasks.audio.AudioClassifier
-AudioClassifierOptions = mp.tasks.audio.AudioClassifierOptions
-AudioClassifierResult = mp.tasks.audio.AudioClassifierResult
-AudioRunningMode = mp.tasks.audio.RunningMode
-BaseOptions = mp.tasks.BaseOptions
-model_path = 'model/yamnet.tflite' # Path to the model file
 
-  
-options = AudioClassifierOptions(
-    base_options=BaseOptions(model_asset_path=model_path),
-    running_mode=AudioRunningMode.AUDIO_CLIPS,
-    max_results=5
-)
+class Classifier():
+    def __init__(self, app):
+        self.AudioClassifier = mp.tasks.audio.AudioClassifier
+        self.AudioClassifierOptions = mp.tasks.audio.AudioClassifierOptions
+        self.AudioClassifierResult = mp.tasks.audio.AudioClassifierResult
+        self.AudioRunningMode = mp.tasks.audio.RunningMode
+        self.BaseOptions = mp.tasks.BaseOptions
+        self.model_path = 'model/yamnet.tflite' # Path to the model file
 
-def start_classifier():
-    with AudioClassifier.create_from_options(options) as classifier:
-        # The 'classify_input_audio' function handles audio recording and classification.
 
-        def classify_input_audio():
-            """
-            Records short audio samples, processes them via AudioClassifier, 
-            and returns a list of category names.
+        self.options = self.AudioClassifierOptions(
+            base_options=self.BaseOptions(model_asset_path=self.model_path),
+            running_mode=self.AudioRunningMode.AUDIO_CLIPS,
+            max_results=5,
+        )
 
-            :return: A list of string category names recognized for the recorded audio.
-            """
-            # Start recording using specified sample rate and duration.
-            rec = Recorder(rate=44100, record_seconds=2, chunksize=1024)
-            # Fetch audio data into a numpy buffer.
-            buffer = rec.record_buffer()
-            audio_data = mp.tasks.components.containers.AudioData.create_from_array(
-                buffer.astype(float)/np.iinfo(np.int16).max, 
-                rec.rate
-            )
-            result = classifier.classify(audio_data)
-            
-            """
-            result = [ClassificationResult(classifications=[Classifications(categories=[Category(index=132, score=0.19921875, display_name='', category_name='Music'), Category(index=133, score=0.08203125, display_name='', category_name='Musical instrument'), Category(index=158, score=0.05859375, display_name='', category_name='Drum machine'), Category(index=500, score=0.05859375, display_name='', category_name='Inside, small room'), Category(index=153, score=0.05859375, display_name='', category_name='Synthesizer')], head_index=0, head_name='scores')], timestamp_ms=0), ClassificationResult(classifications=[Classifications(categories=[Category(index=132, score=0.66796875, display_name='', category_name='Music'), Category(index=159, score=0.19921875, display_name='', category_name='Drum'), Category(index=156, score=0.19921875, display_name='', category_name='Percussion'), Category(index=133, score=0.1484375, display_name='', category_name='Musical instrument'), Category(index=168, score=0.08203125, display_name='', category_name='Wood block')], head_index=0, head_name='scores')], timestamp_ms=975), ClassificationResult(classifications=[Classifications(categories=[Category(index=132, score=0.08203125, display_name='', category_name='Music'), Category(index=494, score=0.05859375, display_name='', category_name='Silence'), Category(index=498, score=0.04296875, display_name='', category_name='Sound effect'), Category(index=0, score=0.01953125, display_name='', category_name='Speech'), Category(index=153, score=0.01171875, display_name='', category_name='Synthesizer')], head_index=0, head_name='scores')], timestamp_ms=1950)]
-            -->result is a list of ClasificationResult objects
 
-            ClassificationResult(classifications=[Classifications(categories=[Category(index=132, score=0.19921875, display_name='', category_name='Music'), Category(index=133, score=0.08203125, display_name='', category_name='Musical instrument'), Category(index=158, score=0.05859375, display_name='', category_name='Drum machine'), Category(index=500, score=0.05859375, display_name='', category_name='Inside, small room'), Category(index=153, score=0.05859375, display_name='', category_name='Synthesizer')], head_index=0, head_name='scores')], timestamp_ms=0)
-            -->result[n] is a ClassificationResult object with classifications attribute
-            --> result[0].classifications is a list of Classifications objects
+    """
+    def start_classifier(self):
+        with self.AudioClassifier.create_from_options(self.options) as classifier:
+            # The 'classify_input_audio' function handles audio recording and classification.
+    """
+    def classify_input_audio(self, classifier):
+        """
+        Records short audio samples, processes them via AudioClassifier, 
+        and returns a list of category names.
 
-            Classifications(categories=[Category(index=132, score=0.19921875, display_name='', category_name='Music'), Category(index=133, score=0.08203125, display_name='', category_name='Musical instrument'), Category(index=158, score=0.05859375, display_name='', category_name='Drum machine'), Category(index=500, score=0.05859375, display_name='', category_name='Inside, small room'), Category(index=153, score=0.05859375, display_name='', category_name='Synthesizer')], head_index=0, head_name='scores')
-            -->result[0].classifications[0].categories is a list of Category objects
-
-            Category(index=132, score=0.19921875, display_name='', category_name='Music')
-            -->result[0].classifications[0].categories[0].category_name is a tuple with timestamp and category name
-            """
-
-            # Close the recorder to release resources.
-            rec.close()
-
-            # Extract timestamp and corresponding category name from result.
-            result_categories = [
-                (r.timestamp_ms, cat.category_name)
-                for r in result
-                if r.classifications
-                for classification in r.classifications
-                if classification.categories
-                for cat in classification.categories
-            ]
-            return [cat_name for _, cat_name in result_categories]
+        :return: A list of string category names recognized for the recorded audio.
+        """
+        # Start recording using specified sample rate and duration.
+        rec = Recorder(rate=44100, record_seconds=2, chunksize=1024)
+        # Fetch audio data into a numpy buffer.
+        buffer = rec.record_buffer()
+        audio_data = mp.tasks.components.containers.AudioData.create_from_array(
+            buffer.astype(float)/np.iinfo(np.int16).max, 
+            rec.rate
+        )
+        result = classifier.classify(audio_data)
         
-        # Loop for multiple classifications, printing categorization each time.
-        for i in range(5):
-            print(f"Classification {i+1}:\n{classify_input_audio()}\n\n")
+        """
+        result = [ClassificationResult(classifications=[Classifications(categories=[Category(index=132, score=0.19921875, display_name='', category_name='Music'), Category(index=133, score=0.08203125, display_name='', category_name='Musical instrument'), Category(index=158, score=0.05859375, display_name='', category_name='Drum machine'), Category(index=500, score=0.05859375, display_name='', category_name='Inside, small room'), Category(index=153, score=0.05859375, display_name='', category_name='Synthesizer')], head_index=0, head_name='scores')], timestamp_ms=0), ClassificationResult(classifications=[Classifications(categories=[Category(index=132, score=0.66796875, display_name='', category_name='Music'), Category(index=159, score=0.19921875, display_name='', category_name='Drum'), Category(index=156, score=0.19921875, display_name='', category_name='Percussion'), Category(index=133, score=0.1484375, display_name='', category_name='Musical instrument'), Category(index=168, score=0.08203125, display_name='', category_name='Wood block')], head_index=0, head_name='scores')], timestamp_ms=975), ClassificationResult(classifications=[Classifications(categories=[Category(index=132, score=0.08203125, display_name='', category_name='Music'), Category(index=494, score=0.05859375, display_name='', category_name='Silence'), Category(index=498, score=0.04296875, display_name='', category_name='Sound effect'), Category(index=0, score=0.01953125, display_name='', category_name='Speech'), Category(index=153, score=0.01171875, display_name='', category_name='Synthesizer')], head_index=0, head_name='scores')], timestamp_ms=1950)]
+        -->result is a list of ClasificationResult objects
 
-    print("\n\nStopped Classifier. End of Program.")
+        ClassificationResult(classifications=[Classifications(categories=[Category(index=132, score=0.19921875, display_name='', category_name='Music'), Category(index=133, score=0.08203125, display_name='', category_name='Musical instrument'), Category(index=158, score=0.05859375, display_name='', category_name='Drum machine'), Category(index=500, score=0.05859375, display_name='', category_name='Inside, small room'), Category(index=153, score=0.05859375, display_name='', category_name='Synthesizer')], head_index=0, head_name='scores')], timestamp_ms=0)
+        -->result[n] is a ClassificationResult object with classifications attribute
+        --> result[0].classifications is a list of Classifications objects
+
+        Classifications(categories=[Category(index=132, score=0.19921875, display_name='', category_name='Music'), Category(index=133, score=0.08203125, display_name='', category_name='Musical instrument'), Category(index=158, score=0.05859375, display_name='', category_name='Drum machine'), Category(index=500, score=0.05859375, display_name='', category_name='Inside, small room'), Category(index=153, score=0.05859375, display_name='', category_name='Synthesizer')], head_index=0, head_name='scores')
+        -->result[0].classifications[0].categories is a list of Category objects
+
+        Category(index=132, score=0.19921875, display_name='', category_name='Music')
+        -->result[0].classifications[0].categories[0].category_name is a tuple with timestamp and category name
+        """
+
+        # Close the recorder to release resources.
+        rec.close()
+
+        # Extract timestamp and corresponding category name from result.
+        result_categories = [
+            (r.timestamp_ms, cat.category_name)
+            for r in result
+            if r.classifications
+            for classification in r.classifications
+            if classification.categories
+            for cat in classification.categories
+        ]
+        return [cat_name for _, cat_name in result_categories]
+        
 
 
 
--- ui/tk_ui.py
+++ ui/tk_ui.py
@@ -1,13 +1,15 @@
 import tkinter as tk
-from app import BellAudioClassifierWebUI
+from classifier import Classifier
+import time
+
 
 class BellAudioClassifierUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Bell Audio Classifier")
         
-        self.running = BellAudioClassifierUI.running
-        self.made_shots = BellAudioClassifierUI.made_shots
+        self.running = False
+        self.made_shots = 0
         
         self.start_button = tk.Button(root, text="Start", font=("Helvetica", 24), command=self.start)
         self.start_button.pack(expand=True)
@@ -16,6 +18,8 @@ class BellAudioClassifierUI:
         self.running = True
         self.start_button.pack_forget()
         
+        self.description_label = tk.Label(self.root, text="Made Shots", font=("Helvetica", 48))
+        self.description_label.pack(expand=True)
         self.made_shots_label = tk.Label(self.root, text=str(self.made_shots), font=("Helvetica", 48))
         self.made_shots_label.pack(expand=True)
         
@@ -24,15 +28,53 @@ class BellAudioClassifierUI:
         
         self.quit_button = tk.Button(self.root, text="Quit", font=("Helvetica", 18), command=self.root.quit)
         self.quit_button.pack(side=tk.RIGHT, expand=True)
+
+        """
+        with c.AudioClassifier.create_from_options(c.options) as classifier:
+            i = 0
+            while self.running:
+                if 'Bell' in c.classify_input_audio(classifier):
+                    self.increase_made_shots()
+        """
+        self.classifier_instance = c.AudioClassifier.create_from_options(c.options)
+        self.classify_audio()
+
+    def classify_audio(self):
+        if not self.running:
+            self.classifier_instance.close()
+            return
+        if "Bell" in c.classify_input_audio(self.classifier_instance):
+            self.increase_made_shots()
+            time.sleep(2)
+            self.root.after(100, self.classify_audio)
+        else:
+            time.sleep(0.5)
+            self.root.after(0, self.classify_audio)
         
+        print(c.classify_input_audio(self.classifier_instance))
+
+    
+    def increase_made_shots(self):
+        self.made_shots += 1
+        self.made_shots_label.config(text=str(self.made_shots))
+
     def stop(self):
         self.running = False
         self.made_shots_label.pack_forget()
+        self.description_label.pack_forget()
         self.stop_button.pack_forget()
         self.quit_button.pack_forget()
         self.start_button.pack(expand=True)
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = BellAudioClassifierUI(root)
-    root.mainloop()
+class App:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.attributes("-topmost", True)
+        self.root.minsize(400, 400)
+        self.app = BellAudioClassifierUI(self.root)
+    
+    def run(self):
+        self.root.mainloop()
+
+app = App()
+c = Classifier(app)
